3eeb2b8dd7ac957a13f7a696424c2e74390825a5
Anthony Lee <anthony.mi.lee@outlook.com>
2018-07-18 14:08:31 -0500

    Created an internal itemListener

--- app/src/main/java/com/example/anthony/basictodoapp/tasks/TasksFragment.kt
+++ app/src/main/java/com/example/anthony/basictodoapp/tasks/TasksFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import com.example.anthony.basictodoapp.data.Task
 
 class TasksFragment : Fragment(), TasksContract.View {
     override lateinit var presenter: TasksContract.Presenter
@@ -19,5 +20,28 @@ class TasksFragment : Fragment(), TasksContract.View {
     private lateinit var tasksView: LinearLayout
     private lateinit var filteringLabelView: TextView
 
+    // Listener for clicks on tasks in the view
+    internal var itemListener: TaskItemListener = object : TaskItemListener {
+        override fun onTaskClick(clickedTask: Task) {
+            presenter.openTaskDetails(clickedTask)
+        }
 
+        override fun onCompleteTskClick(completedTask: Task) {
+            presenter.completeTask(completedTask)
+        }
+
+        override fun onActivateTaskClick(activatedTask: Task) {
+            presenter.activateTask(activatedTask)
+        }
+    }
+
+
+
+    interface TaskItemListener {
+        fun onTaskClick(clickedTask: Task)
+
+        fun onCompleteTskClick(completeTask: Task)
+
+        fun onActivateTaskClick(activatedTask: Task)
+    }
 }
