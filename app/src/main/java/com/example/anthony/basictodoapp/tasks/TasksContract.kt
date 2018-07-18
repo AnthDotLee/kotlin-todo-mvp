d6561148a5c3e07ac74997df752205cf84c98744
Anthony Lee <anthony.mi.lee@outlook.com>
2018-07-18 09:17:47 -0500

    Added the rest of the contract functions

--- app/src/main/java/com/example/anthony/basictodoapp/tasks/TasksContract.kt
+++ app/src/main/java/com/example/anthony/basictodoapp/tasks/TasksContract.kt
@@ -1,2 +1,60 @@
 package com.example.anthony.basictodoapp.tasks
 
+import com.example.anthony.basictodoapp.BasePresenter
+import com.example.anthony.basictodoapp.BaseView
+import com.example.anthony.basictodoapp.data.Task
+
+interface TasksContract {
+
+    interface View : BaseView<Presenter> {
+        var isActive: Boolean
+
+        fun setLoadingIndicator(active: Boolean)
+
+        fun showAddTask()
+
+        fun showTaskDetailsUi(taskId: String)
+
+        fun showTaskMarkedComplete()
+
+        fun showTaskMarkedActive()
+
+        fun showCompletedTasksCleared()
+
+        fun showLoadingTasksError()
+
+        fun showNoTasks()
+
+        fun showActiveFilterLabel()
+
+        fun showCompletedFilterLabel()
+
+        fun showAllFilterLabel()
+
+        fun showNoActiveTasks()
+
+        fun showNoCompletedTasks()
+
+        fun showSuccessfullySavedMessage()
+
+        fun showFilteringPopUpMenu()
+    }
+
+    interface Presenter : BasePresenter {
+        var currentFiltering: TasksFilterType
+
+        fun result(requestCode: Int, resultCode: Int)
+
+        fun loadTasks(forceUpdate: Boolean)
+
+        fun addNewTask()
+
+        fun openTaskDetails(requestedTask: Task)
+
+        fun completeTask(completedTask: Task)
+
+        fun activateTask(activeTask: Task)
+
+        fun clearCompletedTasks()
+    }
+}
---
+++ app/src/main/java/com/example/anthony/basictodoapp/tasks/TasksFilterType.kt
@@ -0,0 +1,6 @@
+package com.example.anthony.basictodoapp.tasks
+
+// Enum for various filtering types. All tasks, completed ones, or remaining tasks
+enum class TasksFilterType {
+    ALL_TASKS, ACTIVE_TASKS, COMPLETED_TASKS
+}
