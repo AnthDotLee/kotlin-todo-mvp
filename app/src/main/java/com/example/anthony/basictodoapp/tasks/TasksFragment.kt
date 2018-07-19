c42b5e19969c0d1d66d07e3d15296a41a4022e24
Anthony Lee <anthony.mi.lee@outlook.com>
2018-07-18 20:59:14 -0500

    wiring string messages and creating some private functions and extensions

--- app/src/main/java/com/example/anthony/basictodoapp/tasks/TasksFragment.kt
+++ app/src/main/java/com/example/anthony/basictodoapp/tasks/TasksFragment.kt
@@ -1,12 +1,16 @@
 package com.example.anthony.basictodoapp.tasks
 
+import android.content.Intent
+import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
+import android.support.v4.widget.SwipeRefreshLayout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import com.example.anthony.basictodoapp.R
 import com.example.anthony.basictodoapp.data.Task
+import com.example.anthony.basictodoapp.util.showSnackBar
 import org.w3c.dom.Text
 
 class TasksFragment : Fragment(), TasksContract.View {
@@ -41,51 +45,59 @@ class TasksFragment : Fragment(), TasksContract.View {
     private val listAdapter = TaskAdapter(ArrayList(0), itemListener)
 
     override fun setLoadingIndicator(active: Boolean) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val root = view ?: return
+        with(root.findViewById < SwipeRefreshLayout(R.id.refresh_layout)) {
+            post { isRefreshing = active }
+        }
     }
 
     override fun showAddTask() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val intent = Intent(context, AddEditTaskActivity::class.java)
+        startActivityForResult(TaskDetailActivity.EXTRA_TASK_ID, taskId)
+        startActivity(intent)
     }
 
     override fun showTaskDetailsUi(taskId: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        // Apparently this sets up for something called "Intent Subbing"
+        val intent = Intent(context, TaskDetailActivity::class.java).apply {
+            putExtra(TaskDetailActivity.EXTRA_TASK_ID, teskId)
+        }
     }
 
     override fun showTaskMarkedComplete() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        showMessage(getString(R.string.task_marked_complete))
     }
 
     override fun showTaskMarkedActive() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        showMessage(getString(R.string.task_marked_active))
     }
 
     override fun showCompletedTasksCleared() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        showMessage(getString(R.string.completed_tasks_cleared))
     }
 
     override fun showLoadingTasksError() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        showMessage(getString(R.string.loading_tasks_error))
     }
 
     override fun showNoTasks() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        showNoTasksViews(resources.getString(R.string.no_tasks_completed), R.drawable.ic_assignment_turned_in_24dp, false)
     }
 
     override fun showActiveFilterLabel() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        filteringLabelView.text = resources.getString(R.string.label_active)
     }
 
     override fun showCompletedFilterLabel() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        filteringLabelView.text = resources.getString(R.string.label_completed)
     }
 
     override fun showAllFilterLabel() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        filteringLabelView.text = resources.getString(R.string.label_all)
     }
 
     override fun showNoActiveTasks() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        showNoTasksViews(resources.getString(R.string.no_tasks_active), R.drawable.ic_check_circle - 24 dp, false)
     }
 
     override fun showNoCompletedTasks() {
@@ -100,8 +112,29 @@ class TasksFragment : Fragment(), TasksContract.View {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
+    private fun showNoTasksViews(mainText: String, iconRes: Int, showAddView: Boolean) {
+        tasksView.visibility = View.GONE
+        noTasksView.visibility = View.VISIBLE
+
+        noTaskMainView.text = mainText
+        noTaskIcon.setImageResource(iconRes)
+        noTaskAddView.visibility = if (showAddView) View.VISIBLE else View.GONE
+    }
+
+    private fun showMessage(message: String) {
+        /*
+        Without using an extention function, you would need to import the Snackbar class and make it manually. Looks something like this:
+
+        import android.support.design.widget.Snackbar
+        ----
+
+        Snackbar.make(view, message, Snackbar.LENGTH_LONG).show()
+         */
+        view?.showSnackBar(message, Snackbar.LENGTH_LONG)
+    }
+
     private class TaskAdapter(tasks: List<Task>, private val itemListener: TaskItemListener) :
-        BaseAdapter() {
+            BaseAdapter() {
 
         var tasks: List<Task> = tasks
             set(tasks) {
@@ -118,9 +151,9 @@ class TasksFragment : Fragment(), TasksContract.View {
         override fun getView(i: Int, view: View?, viewGroup: ViewGroup): View {
             val task = getItem(i)
             val rowView = view ?: LayoutInflater.from(viewGroup.context).inflate(
-                R.layout.task_item,
-                viewGroup,
-                false
+                    R.layout.task_item,
+                    viewGroup,
+                    false
             )
 
             // The below block is the same as doing this:
@@ -133,18 +166,18 @@ class TasksFragment : Fragment(), TasksContract.View {
                 // active/completed task ui
                 isChecked = task.isCompleted
                 val rowViewBackground =
-                    if (task.isCompleted) R.drawable.list_completed_touch_feedback
-                    else R.drawable.touch_feedback
+                        if (task.isCompleted) R.drawable.list_completed_touch_feedback
+                        else R.drawable.touch_feedback
                 rowView.setBackgroundResource(rowViewBackground)
                 setOnClickListener {
-                    if(!task.isCompleted) {
+                    if (!task.isCompleted) {
                         itemListener.onCompleteTaskClick(task)
                     } else {
                         itemListener.onActivateTaskClick(task)
                     }
                 }
             }
-            rowView.setOnClickListener { itemListener. onTaskClick(task)}
+            rowView.setOnClickListener { itemListener.onTaskClick(task) }
             return rowView
         }
     }
