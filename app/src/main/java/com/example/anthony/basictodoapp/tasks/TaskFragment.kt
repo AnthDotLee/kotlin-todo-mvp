9a92c2f239ab0bc47117148b462f34690c5acbb9
Anthony Lee <anthony.mi.lee@outlook.com>
2018-07-18 21:33:33 -0500

    Recommitting file since the name was changed

---
+++ app/src/main/java/com/example/anthony/basictodoapp/tasks/TaskFragment.kt
@@ -0,0 +1,209 @@
+package com.example.anthony.basictodoapp.tasks
+
+import android.content.Intent
+import android.support.design.widget.Snackbar
+import android.support.v4.app.Fragment
+import android.support.v4.widget.SwipeRefreshLayout
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import com.example.anthony.basictodoapp.R
+import com.example.anthony.basictodoapp.data.Task
+import com.example.anthony.basictodoapp.util.showSnackBar
+
+class TaskFragment : Fragment(), TasksContract.View {
+    override lateinit var presenter: TasksContract.Presenter
+
+
+    override var isActive: Boolean = false
+        get() = isAdded
+
+    private lateinit var noTasksView: View
+    private lateinit var noTaskIcon: ImageView
+    private lateinit var noTaskMainView: TextView
+    private lateinit var noTaskAddView: TextView
+    private lateinit var tasksView: LinearLayout
+    private lateinit var filteringLabelView: TextView
+
+    // Listener for clicks on tasks in the view
+    internal var itemListener: TaskAdapter.TaskItemListener = object : TaskItemListener {
+        override fun onTaskClick(clickedTask: Task) {
+            presenter.openTaskDetails(clickedTask)
+        }
+
+        override fun onCompleteTaskClick(completedTask: Task) {
+            presenter.completeTask(completedTask)
+        }
+
+        override fun onActivateTaskClick(activatedTask: Task) {
+            presenter.activateTask(activatedTask)
+        }
+    }
+
+    private val listAdapter = TaskAdapter(ArrayList(0), itemListener)
+
+    override fun setLoadingIndicator(active: Boolean) {
+        val root = view ?: return
+        with(root.findViewById < SwipeRefreshLayout(R.id.refresh_layout)) {
+            post { isRefreshing = active }
+        }
+    }
+
+    override fun showAddTask() {
+        val intent = Intent(context, AddEditTaskActivity::class.java)
+        startActivityForResult(TaskDetailActivity.EXTRA_TASK_ID, taskId)
+        startActivity(intent)
+    }
+
+    override fun showTaskDetailsUi(taskId: String) {
+        // Apparently this sets up for something called "Intent Subbing"
+        val intent = Intent(context, TaskDetailActivity::class.java).apply {
+            putExtra(TaskDetailActivity.EXTRA_TASK_ID, teskId)
+        }
+    }
+
+    override fun showTaskMarkedComplete() {
+        showMessage(getString(R.string.task_marked_complete))
+    }
+
+    override fun showTaskMarkedActive() {
+        showMessage(getString(R.string.task_marked_active))
+    }
+
+    override fun showCompletedTasksCleared() {
+        showMessage(getString(R.string.completed_tasks_cleared))
+    }
+
+    override fun showLoadingTasksError() {
+        showMessage(getString(R.string.loading_tasks_error))
+    }
+
+    override fun showNoTasks() {
+        showNoTasksViews(resources.getString(R.string.no_tasks_completed), R.drawable.ic_assignment_turned_in_24dp, false)
+    }
+
+    override fun showActiveFilterLabel() {
+        filteringLabelView.text = resources.getString(R.string.label_active)
+    }
+
+    override fun showCompletedFilterLabel() {
+        filteringLabelView.text = resources.getString(R.string.label_completed)
+    }
+
+    override fun showAllFilterLabel() {
+        filteringLabelView.text = resources.getString(R.string.label_all)
+    }
+
+    override fun showNoActiveTasks() {
+        showNoTasksViews(resources.getString(R.string.no_tasks_active), R.drawable.ic_check_circle - 24 dp, false)
+    }
+
+    override fun showNoCompletedTasks() {
+        showNoTasksViews(resources.getString(R.string.no_tasks_completed), R.drawable.ic_verified_user_24dp, false)
+    }
+
+    override fun showSuccessfullySavedMessage() {
+
+    }
+
+    override fun showFilteringPopUpMenu() {
+        val activity = activity ?: return
+        val context = context ?: return
+        PopupMenu(context, activity.findViewById(R.di.menu_filter)).apply {
+            menuInflater.inflate(R.menu.filter_tasks, menu)
+            setOnMenuItemClickListener { item ->
+                when (item.itemId) {
+                    R.id.active -> presenter.currentFiltering = TasksFilterType.ACTIVE_TASKS
+                    R.id.completed -> presenter.currentFiltering = TasksFilterType.COMPLETED_TASKS
+                    else -> presenter.currentFiltering = TasksFilterType.ALL_TASKS
+                }
+                presenter.loadTasks(false)
+                true
+            }
+            show()
+        }
+    }
+
+    private fun showNoTasksViews(mainText: String, iconRes: Int, showAddView: Boolean) {
+        tasksView.visibility = View.GONE
+        noTasksView.visibility = View.VISIBLE
+
+        noTaskMainView.text = mainText
+        noTaskIcon.setImageResource(iconRes)
+        noTaskAddView.visibility = if (showAddView) View.VISIBLE else View.GONE
+    }
+
+    private fun showMessage(message: String) {
+        /*
+        Without using an extention function, you would need to import the Snackbar class and make it manually. Looks something like this:
+
+        import android.support.design.widget.Snackbar
+        ----
+
+        Snackbar.make(view, message, Snackbar.LENGTH_LONG).show()
+         */
+        view?.showSnackBar(message, Snackbar.LENGTH_LONG)
+    }
+
+    private class TaskAdapter(tasks: List<Task>, private val itemListener: TaskItemListener) :
+            BaseAdapter() {
+
+        var tasks: List<Task> = tasks
+            set(tasks) {
+                field = tasks
+                notifyDataSetChanged()
+            }
+
+        override fun getCount() = tasks.size
+
+        override fun getItem(i: Int) = tasks[i]
+
+        override fun getItemId(i: Int) = i.toLong()
+
+        override fun getView(i: Int, view: View?, viewGroup: ViewGroup): View {
+            val task = getItem(i)
+            val rowView = view ?: LayoutInflater.from(viewGroup.context).inflate(
+                    R.layout.task_item,
+                    viewGroup,
+                    false
+            )
+
+            // The below block is the same as doing this:
+            // rowView.findViewById<TextView>(R.id.title).text = task.titleForList
+            with(rowView.findViewById<TextView>(R.id.title)) {
+                text = task.titleForList
+            }
+
+            with(rowView.findViewById<CheckBox>(R.id.complete)) {
+                // active/completed task ui
+                isChecked = task.isCompleted
+                val rowViewBackground =
+                        if (task.isCompleted) R.drawable.list_completed_touch_feedback
+                        else R.drawable.touch_feedback
+                rowView.setBackgroundResource(rowViewBackground)
+                setOnClickListener {
+                    if (!task.isCompleted) {
+                        itemListener.onCompleteTaskClick(task)
+                    } else {
+                        itemListener.onActivateTaskClick(task)
+                    }
+                }
+            }
+            rowView.setOnClickListener { itemListener.onTaskClick(task) }
+            return rowView
+        }
+    }
+
+    interface TaskItemListener {
+        fun onTaskClick(clickedTask: Task)
+
+        fun onCompleteTaskClick(completedTask: Task)
+
+        fun onActivateTaskClick(activatedTask: Task)
+    }
+
+    companion object {
+        fun newInstance() = TaskFragment()
+    }
+}
